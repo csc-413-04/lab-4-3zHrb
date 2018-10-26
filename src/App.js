d35e42dac3a86fefa372b67fe5a56d50b84f9b11
Abdulaziz Alharbi <abdulazizalharbi@sfs-wifi-dhcp-10-143-248-106.wifi.sfsu.edu>
2018-10-25 20:58:26 -0700

    final commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,59 @@ import { connect } from 'react-redux';
 import { doTest } from './redux/actions';
 
 class App extends Component {
+
+  constructor(props){
+
+super(props);
+this.state ={
+
+
+color: 'black',
+banner: 'hello',
+isOpen: false,
+
+
+
+};
+this.buttonHandler = this.buttonHandler.bind(this)
+  }
+buttonHandler(){
+
+//console.log('button just got prssed')
+
+this.setState({
+
+  isOpen: !this.state.isOpen,
+
+});
+
+}
+
   render() {
+    let myVarible = <h2>Aziz</h2>
+let myBanner;
+if(this.state.isOpen){
+myBanner = <h1>{this.state.banner}</h1>
+
+}
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+           
+           Abdulaziz
+
           </p>
-          <h2>413 Starter Kit </h2>
+          <h2>{this.state.banner} </h2>
+          <button onClick={this.buttonHandler}> Click me </button>
         </header>
       </div>
     );
   }
 }
+
 
 const mapStateToProps = (state, ownProps) => {
   return {
